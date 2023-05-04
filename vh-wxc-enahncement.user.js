8bcea2e4e6528bdd3194e22ffcf204ed5a1a5cb5
Anthony Holloway <avholloway@gmail.com>
2023-05-04 10:03:19 -0500

    Update vh-wxc-enahncement.user.js
    added status message toast
    used a function to find phone props instead of hard coding to a specific element index
    abstracted some of the code out into functions

--- vh-wxc-enahncement.user.js
+++ vh-wxc-enahncement.user.js
@@ -73,53 +73,105 @@ const device_search_page = () => {
     }, true);
 }
 
-const do_apply_config = () => {
-    console.log('apply config button clicked');
-    $('p-tablecheckbox').each(function(index) {
-        const phone = $(this)[0].__ngContext__[26] || {};
-        if (phone.deviceType === 'ipphone') {
-            const checkbox = $('input[type="checkbox"]', $(this))[0];
-            if (checkbox.checked) {
-                console.log(`resyncing ${phone.displayName}`);
-                GM_xmlhttpRequest({
-                    method: 'POST',
-                    url: `https://webexapis.com/v1/telephony/config/devices/${phone.deviceId}/actions/applyChanges/invoke`,
-                    headers: {
-                        'Authorization': `Bearer ${access_token}`,
-                        'Content-Type': 'application/json'
-                    }
-                });
-            }
+const find_phone_props = (element) => {
+    const props = $(element)[0].__ngContext__;
+    for (let i = 0, j = props.length; i < j; i++) {
+        if (props[i] && typeof props[i] === 'object' && 'deviceType' in props[i]) {
+            return props[i];
         }
+    }
+}
+
+const is_ipphone = phone => phone.deviceType === 'ipphone';
+
+const is_checked = element => $('input[type="checkbox"]', $(element))[0].checked;
+
+const get_selected_phones = () => {
+    const phones = [];
+    $('p-tablecheckbox').each(function(index) {
+        const phone = find_phone_props(this);
+        if (is_ipphone(phone) && is_checked(this))
+            phones.push(phone);
     });
+    return phones;
+}
+
+const hide_status = () => {
+    $('div#my-custom-status').remove();
+}
+
+const show_status = (message) => {
+    const html = `
+        <div dir="ltr" id="my-custom-status">
+            <div id="cdk-overlay-0" class="cdk-overlay-pane">
+                <md-alert-container role="alert" class="md-alert__container md-alert__container--bottom-right">
+                    <md-alert role="alert" _nghost-pnv-c103="" aria-label="success" class="md-alert md-alert--success">
+                        <div _ngcontent-pnv-c103="" aria-hidden="true" class="md-alert__icon"></div>
+                        \x3C!---->
+                        <div _ngcontent-pnv-c103="" aria-live="polite" class="md-alert__content">
+                            <h3 _ngcontent-pnv-c103="" class="md-alert__title"></h3>
+                            <div _ngcontent-pnv-c103="" class="md-alert__messages">
+                                <div _ngcontent-pnv-c103="" class="md-alert__message">${message}</div>
+                                \x3C!---->\x3C!---->\x3C!---->\x3C!---->\x3C!---->
+                            </div>
+                            \x3C!---->
+                        </div>
+                        <div _ngcontent-pnv-c103="" class="md-alert__button">
+                            <md-icon _ngcontent-pnv-c103="" name="cancel" clickable="true" style="display: inline-flex;" aria-labelledby="">
+                                <button mdbutton="" class="md-button md-button--32 md-button--icon" aria-label="Close" active="false" href="" tabindex="0" type="button">
+                                    \x3C!----><span class="md-button__children" style="opacity: 1;"><i class="icon icon-cancel_16 md-icon" style="color: inherit;"></i></span>
+                                </button>
+                                \x3C!---->
+                            </md-icon>
+                        </div>
+                        \x3C!---->
+                    </md-alert>
+                    \x3C!---->
+                </md-alert-container>
+            </div>
+        </div>
+    `;
+    $(html).insertBefore('div#walkme-dummy-last-element');
+    $('div#my-custom-status md-icon').click(function() { hide_status() });
+}
+
+const do_apply_config = () => {
+    const phones = get_selected_phones();
+    show_status(`Apply Config command will be sent to ${phones.length} phones`);
+    for (const phone of phones) {
+        console.log(`resyncing ${phone.displayName}`);
+        GM_xmlhttpRequest({
+            method: 'POST',
+            url: `https://webexapis.com/v1/telephony/config/devices/${phone.deviceId}/actions/applyChanges/invoke`,
+            headers: {
+                'Authorization': `Bearer ${access_token}`,
+                'Content-Type': 'application/json'
+            }
+        });
+    }
 }
 
 const do_reboot_phones = () => {
-    console.log('reboot phones button clicked');
-    $('p-tablecheckbox').each(function(index) {
-        const phone = $(this)[0].__ngContext__[26] || {};
-        if (phone.deviceType === 'ipphone') {
-            const checkbox = $('input[type="checkbox"]', $(this))[0];
-            if (checkbox.checked) {
-                console.log(`rebooting ${phone.displayName}`);
-                const device_id = phone.wdmUrl.split('\/').pop();
-                const encoded_device_id = btoa(`ciscospark://urn:TEAM:us-west-2_r/DEVICE/${device_id}`);
-                const payload = {
-                    deviceId: encoded_device_id,
-                    arguments: {
-                        'Force': 'False'
-                    }
-                }
-                GM_xmlhttpRequest({
-                    method: 'POST',
-                    url: `https://xapi-r.wbx2.com/xapi/api/xapi/command/SystemUnit.Boot`,
-                    headers: {
-                        'Authorization': `Bearer ${access_token}`,
-                        'Content-Type': 'application/json'
-                    },
-                    data: JSON.stringify(payload)
-                });
+    const phones = get_selected_phones();
+    show_status(`Reboot command will be sent to ${phones.length} phones`);
+    for (const phone of phones) {
+        console.log(`rebooting ${phone.displayName}`);
+        const device_id = phone.wdmUrl.split('\/').pop();
+        const encoded_device_id = btoa(`ciscospark://urn:TEAM:us-west-2_r/DEVICE/${device_id}`);
+        const payload = {
+            deviceId: encoded_device_id,
+            arguments: {
+                'Force': 'False'
             }
         }
-    });
+        GM_xmlhttpRequest({
+            method: 'POST',
+            url: `https://xapi-r.wbx2.com/xapi/api/xapi/command/SystemUnit.Boot`,
+            headers: {
+                'Authorization': `Bearer ${access_token}`,
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(payload)
+        });
+    }
 }
